01686846750a6c325eda1374fe87875f2c439159
Joao Duro <joaodurodei@gmail.com>
2011-09-08 12:40:00 +0100

    first commit on IA

---
+++ IA_Testing/src/ia_testing/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ia_testing;
+
+/**
+ *
+ * @author jduro
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Testing IA...");
+    }
+
+}
